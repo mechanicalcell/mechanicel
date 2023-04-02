d3815306701432bf953868d258067c4e4fd8a05e
mechanicell <mitskevichvyacheslav@gmail.com>
2023-04-02 23:21:44 +0300

    1.1

--- mechanicell/src/App.js
+++ mechanicell/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 
 function importAll(r) {
 
@@ -25,16 +27,29 @@ function App() {
 
   const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
   const [number, setNumber] = React.useState(0);
+  const [number1, setNumber1] = React.useState(9);
   const [button, setButton] = React.useState(11);
   const swap = () => { number <= 41 ? setNumber(number + 1) : setNumber(0) }
   const mouseDown = () => { setButton(12); swap() }
   const mouseUp = () => setButton(11)
-  
+  const onClick = () => { number1 == 9 ? setNumber1(10) : setNumber1(9);                }
+
+  useEffect(() => { if (number1 == 10) {
+    const interval = setInterval(() => { 
+      if (number >= 42) { setNumber(0); } else 
+        setNumber(number => number + 1);
+    }, 100);
+    return () => clearInterval(interval);
+  } else return }, [number1, number]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-button" onMouseDown={mouseDown} onMouseUp={mouseUp}>
+        <div className="App-button1" onMouseDown={mouseDown} onMouseUp={mouseUp}>
           <img src={images[`b${button}.png`]} /> 
+        </div> 
+        <div className="App-button2" onClick={onClick} >
+          <img src={images[`b${number1}.png`]} /> 
         </div> 
       </header>   
       <div className="App-body">
