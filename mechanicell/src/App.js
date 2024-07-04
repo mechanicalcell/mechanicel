f36f564e8bc849c7147fe1e041636da17cf871c6
Ольга Мицкевич <olgamickevic@Mac-mini-Olga.local>
2024-07-05 00:57:25 +0800

    s9

--- mechanicell/src/App.js
+++ mechanicell/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './App.css';
-import { useCallback } from 'react';
 import { useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { useState } from 'react';
+import styles from './app.module.css';
 
-function importAll(r) {
+const importAll = (r) => {
 
   let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+ 
   return images;
 
 }
@@ -19,8 +18,8 @@ const ImageSwap = (number) => {
   const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
   return (
-    <div className='App-image-swap'>
-      <img src={images[`m${number.number}.png`]} draggable="false" /> 
+    <div className={`${styles.app_image_swap}`}>
+      <img src={images[`m${number.number}.png`]} alt={number.number} draggable="false" /> 
     </div>
   )
 
@@ -29,26 +28,42 @@ const ImageSwap = (number) => {
 function App() {
 
   const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
-  const [number, setNumber] = React.useState(0);
-  const [number1, setNumber1] = React.useState(9);
-  const [number2, setNumber2] = React.useState(1);
-  const [number3, setNumber3] = React.useState(1);
-  const [number4, setNumber4] = React.useState(1);
-  const [button, setButton] = React.useState(11);
+
+  const [number, setNumber] = useState(0);
+  const [number1, setNumber1] = useState(9);
+  const [number2, setNumber2] = useState(1);
+  const [number3, setNumber3] = useState(1);
+  const [number4, setNumber4] = useState(1);
+  const [button, setButton] = useState(11);
+
   const swap = () => { number <= 41 ? setNumber(number + 1) : setNumber(0) }
+
   const mouseDown = () => { setButton(12); swap() }
   const mouseUp = () => setButton(11)
-  const mouseDown2 = () => { setNumber2(2);  }
-  const mouseUp2 = () => setNumber2(1)
-  const mouseDown3 = () => { setNumber3(2);  }
-  const mouseUp3 = () => setNumber3(1)
-  const mouseDown4 = () => { setNumber4(2);  }
-  const mouseUp4 = () => setNumber4(1)
+  const mouseDown2 = () => { setNumber2(2); }
+  const mouseUp2 = () => { setNumber2(1); }
+  const mouseDown3 = () => { setNumber3(2); }
+  const mouseUp3 = () => { setNumber3(1); }
+  const mouseDown4 = () => { setNumber4(2); }
+  const mouseUp4 = () => { setNumber4(1); }
 
-  const onClick = () => { number1 == 9 ? setNumber1(10) : setNumber1(9); }
+  const onClick = () => { number1 === 9 ? setNumber1(10) : setNumber1(9); }
 
-  const copyRU = ' 2016 МеханиКл. Все права защищены.';
-  const copyEN = ' 2016 MechanicEl. All rights reserved.';  
+  const copyRU = ' 2016  МеханиКл. Все права защищены.';
+  const copyEN = ' 2016  MechanicEl. All rights reserved.';  
+  
+  const [languageAnimation, setLanguageAnimation] = useState({ animation: '' }); 
+  
+  const cB = () => { 
+    setLanguageAnimation({
+      animation: styles.an
+    });  
+    setTimeout(() => {
+      setLanguageAnimation({
+        animation: ''
+      })
+    }, 1000);
+  }
 
   const savedState =  JSON.parse(localStorage.getItem('state'));
 
@@ -59,19 +74,29 @@ function App() {
     c: savedState ? savedState.c : 'Кл',
     copy: savedState ? savedState.copy : copyRU
   });
-  
-  const cL = () => {
-    'Ру' === state.l ? setState({ l: 'En', d: 'ДИАФИЛЬМ', m: 'Механи', c: 'Кл', copy:    copyRU }) : setState({ l: 'Ру', d: 'FILMSTRIP', m: 'Mechani', c: 'cEl', copy: copyEN })
-  }
 
-  const cB = () => {
-    
+  const changeL = `${styles.app_language} ` === `${styles.app_language} ${languageAnimation.animation}`;
+
+  const cL = () => { 
+    if (changeL) {
+      cB();
+      'Ру' === state.l ? setState({ 
+        l: 'En', 
+        d: 'ДИАФИЛЬМ', 
+        m: 'Механи', 
+        c: 'Кл', 
+        copy: copyRU }) : setState({ 
+          l: 'Ру', 
+          d: 'FILMSTRIP', 
+          m: 'Mechani', 
+          c: 'cEl', 
+          copy: copyEN });
+    }
   }
 
   localStorage.setItem('state', JSON.stringify(state));
-
     
-  useEffect(() => { if (number1 == 10) {
+  useEffect(() => { if (number1 === 10) {
     const interval = setInterval(() => { 
       if (number >= 42) { setNumber(0); } else 
         setNumber(number => number + 1);
@@ -81,52 +106,43 @@ function App() {
 
   return (
     <Router><>
-    <div className="App">
-      <header className="App-header">
-        <div className="App-box">
-        <span id="filmstrip" className="fp">{state.d}</span> 
-        <div className="App-btn12">
-          <div className="App-button1" onMouseDown={mouseDown} onMouseUp={mouseUp}>
-            <img src={images[`b${button}.png`]} draggable="false" /> 
+    <div className={`${styles.app}`} >
+      <header className={`${styles.app_header}`} >
+        <div className={`${styles.app_box}`} >
+        <span className={`${styles.fp} ${languageAnimation.animation}`} >{state.d}</span> 
+        <div className={`${styles.app_btn12}`} >
+          <div className={`${styles.app_button1}`} onMouseDown={mouseDown} onMouseUp={mouseUp} >
+            <img src={images[`b${button}.png`]} alt="" draggable="false" /> 
           </div> 
-          <div className="App-button2" onClick={onClick} >
-            <img src={images[`b${number1}.png`]} draggable="false" /> 
+          <div className={`${styles.app_button2}`} onClick={onClick} >
+            <img src={images[`b${number1}.png`]} alt="" draggable="false" /> 
           </div> 
         </div>
         </div>
-
-        <div className="App-language" onClick={cL} >
-          <span id="cL">{state.l}</span> 
+        <div className={`${styles.app_language} ${languageAnimation.animation}`} onClick={cL} >
+          <span>{state.l}</span> 
         </div> 
-
-        <div className="App-archive" onMouseDown={mouseDown3} onMouseUp={mouseUp3} onClick={cB} >
-          <img src={images[`x${number3}.png`]} draggable="false" />
+        <div className={`${styles.app_archive}`} onMouseDown={mouseDown3} onMouseUp={mouseUp3} >
+          <img src={images[`x${number3}.png`]} alt="" draggable="false" />
         </div> 
-       
-        <Link to='/' className="navigation_link">
-        <div className="App-mask-box" onMouseDown={mouseDown2} onMouseUp={mouseUp2}>
-        
-        <img src={images[`a${number2}.png`]} draggable="false" />
-        
+        <Link to='/' className={`${styles.navigation_link}`}>
+        <div className={`${styles.app_mask_box}`} onMouseDown={mouseDown2} onMouseUp={mouseUp2}>
+        <img src={images[`a${number2}.png`]} alt="" draggable="false" />
         </div>
         </Link>
       </header>   
-      <div className="App-body" >
+      <div className={`${styles.app_body}`} >
         <ImageSwap number={number} />
-        <div className="Mechanicel">
-          <p className="App-about" onMouseDown={mouseDown4} onMouseUp={mouseUp4} style={{padding: 0, margin: 0}}><img src={images[`mm${number4}.png`]} draggable="false" /></p>
-          <h1><span id="h1m1">{state.m}</span><span id="cEl" className="cEl">{state.c}</span></h1>
+        <div className={`${styles.mechanicel} ${languageAnimation.animation}`}>
+          <p className={`${styles.about} ${languageAnimation.animation}`} onMouseDown={mouseDown4} onMouseUp={mouseUp4} style={{padding: 0, margin: 0}}><img src={images[`icon${number4}.svg`]} alt="" draggable="false" /></p>
+          <h1><span className={`${styles.h1m1} ${languageAnimation.animation}`} >{state.m}</span><span className={`${styles.cel} ${languageAnimation.animation}`}>{state.c}</span></h1>
         </div>
       </div>
-
-
-      <div><p style={{textAlign: "center", fontFamily: "'Open Sans', sans-serif"}}><span id="copy" style={{fontSize: "13px", color: "#5e5e5e", cursor: "default"}}>{state.copy}</span></p></div>
-
+      <div><p><span className={`${styles.copy} ${languageAnimation.animation}`}>{state.copy}</span></p></div>
     </div>
     </>
     </Router>
   );
-
 }
 
 export default App;
