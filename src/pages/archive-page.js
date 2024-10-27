aacd553dcc55117c31e007f95977b4e90397bbee
mechanicalcell <viacheslavmitskevich@ya.ru>
2024-10-27 14:21:27 +0800

    Add files via upload

--- src/pages/archive-page.js
+++ src/pages/archive-page.js
@@ -1,5 +1,13 @@
-export function ArchivePage() {
+import styles from './archive-page.module.css';
+import { NavLink } from 'react-router-dom';
+
+export function ArchivePage({ images, style, addStyle }) {
   return (
-    <div>archive</div>
+    <div className={styles.container}>
+      archive
+      <NavLink to='/login' className={`${styles.user} ${style.user}`} onMouseOver={() => addStyle({ home: '', archive: '', user: styles.element_background_color })} onMouseOut={() => addStyle({ home: '', archive: '', user: '' })} onMouseDown={() => addStyle({ home: '', archive: '', user: styles.element_background_color })} onMouseUp={() => addStyle({ home: '', archive: '', user: '' })} onTouchStart={() => addStyle({ home: '', archive: '', user: styles.element_background_color })} onTouchEnd={() => addStyle({ home: '', archive: '', user: '' })}>
+            {({ isActive }) => <><img src={images[`u${isActive ? 2 : 1}.png`]} alt={`user ${isActive ? 2 : 1}`} draggable="false" /></>}
+      </NavLink>
+    </div>
   );
 } 
